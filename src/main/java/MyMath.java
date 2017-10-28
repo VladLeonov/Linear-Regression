e7646f1049c7681be33e054b5278af581242e2e5
Vladislav Leonov <vlad_leonov@bk.ru>
2017-10-28 23:17:26 +0300

    Adding getPrice method

--- src/main/java/MyMath.java
+++ src/main/java/MyMath.java
@@ -13,11 +13,16 @@ public abstract class MyMath {
         for (int i = 0; i < n; i++) {
 
             data = dataSet.get(i);
-            sum += Math.pow(data.area * weightsArray.areaWeight +
-                    data.rooms * weightsArray.roomsWeight +
-                    weightsArray.freeWeight - data.price, 2);
+            sum += Math.pow(getPrice(data, weightsArray) - data.price, 2);
         }
 
         return Math.pow(sum / (n - 1), 0.5);
+    }
+
+    public static double getPrice(Data data, WeightsArray weightsArray) {
+
+        return data.area * weightsArray.areaWeight +
+                data.rooms * weightsArray.roomsWeight +
+                weightsArray.freeWeight;
     }
 }
