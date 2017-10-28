35470762de2873f35c3571813f26f3e6c764b866
Vladislav Leonov <vlad_leonov@bk.ru>
2017-10-28 21:44:00 +0300

    Adding WeightsArray.java

---
+++ src/main/java/WeightsArray.java
@@ -0,0 +1,11 @@
+
+public class WeightsArray {
+
+    double areaWeight, roomsWeight, priceWeight;
+
+    public WeightsArray(double areaWeight, double roomsWeight, double priceWeight) {
+        this.areaWeight = areaWeight;
+        this.roomsWeight = roomsWeight;
+        this.priceWeight = priceWeight;
+    }
+}
