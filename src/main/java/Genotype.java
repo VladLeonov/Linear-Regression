2e54d61f1219633b67a0825747aee9abdc026f34
Vladislav Leonov <vlad_leonov@bk.ru>
2017-10-29 14:45:13 +0300

    Adding a Genotype class

---
+++ src/main/java/Genotype.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+class Genotype {
+
+    WeightsArray genes;
+    ArrayList<Data> dataSet;
+
+    Genotype(ArrayList<Data> dataSet) {
+
+        this.dataSet = dataSet;
+    }
+
+    Genotype(double genesValueBorder, Random random, ArrayList<Data> dataSet) {
+
+        this(dataSet);
+
+        genes = new WeightsArray(2 * genesValueBorder * random.nextDouble() - genesValueBorder,
+                2 * genesValueBorder * random.nextDouble() - genesValueBorder,
+                2 * genesValueBorder * random.nextDouble() - genesValueBorder);
+    }
+
+    Genotype(Genotype firstParent, Genotype secondParent, ArrayList<Data> dataSet) {
+
+        this(dataSet);
+
+        WeightsArray genes1 = firstParent.genes, genes2 = secondParent.genes;
+        genes = new WeightsArray((genes1.areaWeight + genes2.areaWeight) / 2,
+                (genes1.roomsWeight + genes2.roomsWeight) / 2,
+                (genes1.freeWeight + genes2.freeWeight) / 2);//TODO make better
+    }
+
+    void mutate(int geneIndex, double proportionOfScatter, Random random) {
+
+        double valueMultiplier = 1 - proportionOfScatter + (2 * proportionOfScatter) * random.nextDouble();
+        switch (geneIndex) {
+
+            case 0:
+                genes.areaWeight *= valueMultiplier;
+                break;
+            case 1:
+                genes.roomsWeight *= valueMultiplier;
+                break;
+            case 2:
+                genes.freeWeight *= valueMultiplier;
+                break;
+        }
+    }
+
+    int compareTo(Genotype other) {
+        return Double.compare(MyMath.getStandardDeviation(dataSet, genes),
+                MyMath.getStandardDeviation(dataSet, other.genes));
+    }
+}
